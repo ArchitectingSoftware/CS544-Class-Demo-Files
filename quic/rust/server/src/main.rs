aedf6eeed39e120792b376ac9649f8cf6c779dcd
Brian Mitchell <dr.brian.mitchell@gmail.com>
2024-03-19 16:08:47 -0400

    rust example

---
+++ quic/rust/server/src/main.rs
@@ -0,0 +1,28 @@
+
+use s2n_quic::Server;
+use std::{error::Error, path::Path};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut server = Server::builder()
+        .with_tls((Path::new("cert.pem"), Path::new("key.pem")))?
+        .with_io("127.0.0.1:4433")?
+        .start()?;
+
+    while let Some(mut connection) = server.accept().await {
+        // spawn a new task for the connection
+        tokio::spawn(async move {
+            while let Ok(Some(mut stream)) = connection.accept_bidirectional_stream().await {
+                // spawn a new task for the stream
+                tokio::spawn(async move {
+                    // echo any data back to the stream
+                    while let Ok(Some(data)) = stream.receive().await {
+                        stream.send(data).await.expect("stream should be open");
+                    }
+                });
+            }
+        });
+    }
+
+    Ok(())
+}
