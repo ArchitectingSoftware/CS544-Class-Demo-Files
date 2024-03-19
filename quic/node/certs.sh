e78b78b8481f6b535d0e7f4efc1589bb95db1e92
Brian Mitchell <dr.brian.mitchell@gmail.com>
2024-03-19 09:59:13 -0400

    project updates

--- quic/node/certs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-openssl genpkey -algorithm RSA -aes256 -pass pass:drexelnetworking  \
--out private_key.pem \
--config certificate.conf
-
-openssl rsa -pubout -in private_key.pem -out public_key.pem -passin pass:drexelnetworking
